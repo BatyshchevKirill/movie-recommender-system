85eb3c13c2028403a755ebea930f65779a4f0754
Kirill Batyshchev <k.batyshchev@innopolis.university>
2023-12-02 15:36:59 +0300

    Add dataset downloader
    Signed-off-by: Kirill Batyshchev <k.batyshchev@innopolis.university>

---
+++ download_dataset.py
@@ -0,0 +1,22 @@
+import os
+import requests
+from zipfile import ZipFile
+
+DATA_URL = "https://files.grouplens.org/datasets/movielens/ml-100k.zip"
+DEST_FOLDER = "data/raw"
+
+if __name__ == "__main__":
+    response = requests.get(DATA_URL)
+
+    if response.status_code == 200:
+        file_path = DEST_FOLDER + "/" + DATA_URL.split("/")[-1]
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+
+        with ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(DEST_FOLDER)
+
+        os.remove(file_path)
+        print("Downloaded the dataset to", DEST_FOLDER)
+    else:
+        print("Download failed. Code:", {response.status_code})
